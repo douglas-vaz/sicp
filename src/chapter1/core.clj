e607b1c9c719c4634f57d8dd7a56ea5289a8ae43
Douglas Vaz <douglas1992@gmail.com>
2015-08-15 13:05:55 +0530

    Split sqrt into seperate file under same namespace

--- src/chapter1/core.clj
+++ src/chapter1/core.clj
@@ -27,21 +27,4 @@
     0
     y))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn- average [a b] (/ (+ a b) 2))
-
-(defn- abs [x] (if (> x 0) x (- x)))
-
-(defn- improve [guess x]
-  (average guess (/ x guess)))
-
-(defn- good-enough? [guess x]
-  (<= (abs (- x (square guess))) 0.001))
-
-(defn- sqrt-iter [guess x]
-  (if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x) x)))
-
-(defn sqrt [x] (sqrt-iter 1 x))
+(load "core_sqrt")
